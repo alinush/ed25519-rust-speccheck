0e2f145d81eb01adda6827a4d93ef470466bdba5
Alin Tomescu <alinush@mit.edu>
2022-07-20 10:00:41 -0700

    add color coding in ./run.sh

--- run.sh
+++ run.sh
@@ -26,4 +26,39 @@ echo "|[CGN20e] Alg.2 | X | X | V | V | V | V | X | X | X | X | X | X |" >result
 cat results-temp.md >>results.md
 rm results-temp.md
 
-cat results.md
+RED='\\033[0;31m'
+GREEN='\\033[0;32m'
+BOLDPURPLE='\\033[1;35m'
+NC='\\033[0m' # No color
+
+color_output() {
+    sed "s/X/${RED}X${NC}/g" |
+    sed "s/V/${GREEN}V${NC}/g" |
+    sed "s/aptos-crypto/${BOLDPURPLE}aptos-crypto${NC}/g" |
+    sed "s/\[CGN20e\] Alg.2/${GREEN}\[CGN20e] Alg.2${NC}/g"
+}
+
+out=`cat results.md \
+    | grep -v aptos-crypto \
+    | grep -v CGN20e \
+    | grep -v Dalek \
+    | color_output
+`
+out_dalek=`cat results.md \
+    | grep Dalek \
+    | color_output \
+`
+out_aptos=`cat results.md \
+    | grep aptos-crypto \
+    | color_output \
+`
+out_cgn20e=`cat results.md \
+    | grep CGN20e \
+    | color_output \
+`
+echo -e "$out"
+echo "|---------------|-----------------------------------------------|"
+echo -e "$out_dalek"
+echo "|---------------|-----------------------------------------------|"
+echo -e "$out_aptos"
+echo -e "$out_cgn20e"
